2a212894a55d732470b5033675d7f9389253ead1
Roni Andarsyah <roni.andarsyah@gmail.com>
2022-10-28 15:54:31 +0700

    Under Construction

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <p>
           Museum Geologi didirikan pada 16 Mei 1929. Museum ini direnovasi dengan dana bantuan dari JICA. Setelah mengalami renovasi, Museum Geologi dibuka kembali dan diresmikan oleh Wakil Presiden
-          RI, Megawati Soekarnoputri pada 23 Agustus 2000
+          RI
         </p>
         <a className="App-link" href="https://irc.ulbi.ac.id/" target="_blank" rel="noopener noreferrer">
           Informatics Research Center
