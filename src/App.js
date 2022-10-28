0abb48eb9768d95bfa4514415d9b4e04fa082625
Rolly Maulana Awangga <rolly@awang.ga>
2022-10-28 15:18:30 +0700

    add IRC URL

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
         <p>
           Museum Geologi Bandung <code>url/geologi/src/App.js</code> resfrest weh browsernya
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+        <a className="App-link" href="https://irc.ulbi.ac.id/" target="_blank" rel="noopener noreferrer">
+          Informatics Research Center
         </a>
       </header>
     </div>
