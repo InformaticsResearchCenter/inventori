d436e4220af4a771d9e6da7135557f2f2060dd3b
Roni Andarsyah <roni.andarsyah@gmail.com>
2022-10-28 15:23:05 +0700

    nambah iseng

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Museum Geologi Bandung <code>url/geologi/src/App.js</code> resfrest weh browsernya
         </p>
+        <p>
+          Museum Geologi didirikan pada 16 Mei 1929. Museum ini direnovasi dengan dana bantuan dari JICA. Setelah mengalami renovasi, Museum Geologi dibuka kembali dan diresmikan oleh Wakil Presiden
+          RI, Megawati Soekarnoputri pada 23 Agustus 2000
+        </p>
         <a className="App-link" href="https://irc.ulbi.ac.id/" target="_blank" rel="noopener noreferrer">
           Informatics Research Center
         </a>
